4bc451ba8c4f40107432990bf8186f50a084f474
HarshKhandeparkar <mjkgoa@gmail.com>
2019-04-09 19:29:19 +0530

    Updates

---
+++ precache-manifest.2bfaf5fb9cb98dc9931886925ce6fdba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8dcdc25078f48d03916",
+    "url": "/static/js/main.ed187283.chunk.js"
+  },
+  {
+    "revision": "2899c966e04b4feec520",
+    "url": "/static/js/2.bb2b29d9.chunk.js"
+  },
+  {
+    "revision": "d8dcdc25078f48d03916",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "ea6fddf6b5621bb0512751f6d131ce6c",
+    "url": "/index.html"
+  }
+];
